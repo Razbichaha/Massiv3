330205412f9d2e44c94b5b60f12fa2df963007be
Сергей <seregabur@live.ru>
2022-04-07 18:45:45 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Massiv3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Random random = new Random();
+            int minimumValue = 1;
+            int maximumValue = 100;
+            int maximumArrayLength = 30;
+
+            int[] array = new int[maximumArrayLength];
+
+            for(int i=0;i<array.Length;i++ )
+            {
+                array[i] = random.Next(minimumValue, maximumValue);
+            }
+
+            foreach(int i in array)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.Write("\n");
+            int maximumCounter = 0;
+
+            for(int i=0;i<array.Length;i++ )
+            {
+
+                if (i == 0)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        maximumCounter++;
+                        Console.Write("Максимум №" + maximumCounter + " = " + array[i] + "\n");
+                    }
+
+                } else if (i == array.Length - 1)
+                {
+
+                    if (array[i] > array[i - 1])
+                    {
+                        maximumCounter++;
+                        Console.Write("Максимум №" + maximumCounter + " = " + array[i] + "\n");
+                    }
+
+                }else
+                 { 
+
+                    if (array[i]>array[i+1]&array[i]>array[i-1])
+                    {
+                    maximumCounter++;
+                    Console.Write("Максимум №" + maximumCounter + " = " + array[i]+"\n");
+                    }
+
+                 }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
